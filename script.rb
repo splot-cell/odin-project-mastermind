9b03d72b02495895b3c31587d3dac22d9fc6e060
Olly Seber <olly_s@hotmail.co.uk>
2022-08-07 17:35:38 -0700

    Refactor BoardRow class into Code class

--- script.rb
+++ script.rb
@@ -1,15 +1,31 @@
-class BoardRow
-  def initalize
-    @row = Array.new(4)
+class Code
+  def initalize(code)
+    @code = code
+  end
+
+  protected
+  attr_reader :code
+
+  public
+  def equal_to?(target)
+    @code == target.code
   end
 end
 
 class Board
-  def initalize(size, target_code)
-    @rows = Array.new(size, BoardRow.new)
-    @current_row_index = 0
-    @target_code = target_code
+  def initalize(target_code)
+    @rows = []
+    @total_guesses = 0
+    @target_code = Code.new(target_code)
   end
+
+  def current_guess_correct?
+    return if @total_guesses = 0
+    @rows[@total_guesses - 1].equal_to?(@target_code)
+  end
+
+  def submit_guess
+
 end
 
 class Game
@@ -17,11 +33,12 @@ class Game
     master: 0,
     breaker: 1
   }
+  MAX_GUESSES = 12
   def initialize(codemaster_class, codebreaker_class)
     @players = [codemaster_class.new(self, ROLE[:master]),
                 codebreaker_class.new(self, ROLE[:breaker])]
 
-    @board = Board.new(12, @players[ROLE[:master]].generate_code)
+    @board = Board.new(@players[ROLE[:master]].generate_code)
   end
 
   def playable_characters
