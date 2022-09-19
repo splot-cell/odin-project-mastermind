d1e477cce4e5cb6eceb7824ca3cdf63d1ba3c403
Olly Seber <olly_s@hotmail.co.uk>
2022-09-18 22:43:55 -0700

    Create skeleton code for printing board

--- display.rb
+++ display.rb
@@ -9,4 +9,21 @@ module Display
   def clear_console
     system("clear") || system("cls")
   end
+
+  def print_board
+    0.upto(max_guesses) do |i|
+      print_row(i)
+    end
+  end
+
+  def print_row(row_number)
+    print_guess(row_number)
+    print_hint(row_number)
+  end
+
+  def print_guess(guess_number)
+  end
+
+  def print_hint(hint_number)
+  end
 end
